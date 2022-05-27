995cc8d5a9bf8cc39ed77df3cc028b790db0395a
jayeshgit93@gmail.com <jayeshgit93@gmail.com>
2022-05-27 22:30:52 +0530

    react-redux - dispatching action using use selecotr hook of react-redux

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,14 +1,14 @@
 import { createStore } from 'redux'
 const INITIAL_STATE = { counter: 0 };
 const counterReducer = (state = INITIAL_STATE, action) => {
-    if (action.type === 'inc') {
+    if (action.type === 'increment') {
         return {
-            counter: state.counter++
+            counter: state.counter + 1
         }
     }
-    else if (action.type === 'dec') {
+    else if (action.type === 'decrement') {
         return {
-            counter: state.counter--
+            counter: state.counter - 1
         }
 
     }
